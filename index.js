ee093e490065f8b634deff6c62b64f68d13472ac
Harsh Kumar rattewal <91414873+harsh7231@users.noreply.github.com>
2023-07-13 17:43:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function toss(){
+    const choices = ['head', 'tail'];
+    const choice = choices[Math.floor(Math.random() *2)];
+    document.getElementById('result').innerText= choice;
+
+}
+var choices=['Sawan','Sneha','Anshul','Harsh','Jatin','Diyansh','Kishu','Param','Amogh','Aarya','Garvit'];
+var pickedName=[];
+function player(){
+    if(choices.length ===0){
+        result.textContent = "Good luck mates";
+        return;
+    }
+    var randomIndex = Math.floor(Math.random()*choices.length);
+    var randomPlayer = choices[randomIndex];
+    pickedName.push(randomPlayer);
+    choices.splice(randomIndex, 1);
+    result.textContent = randomPlayer;
+}
+function reset() {
+    choices = pickedName.concat(choices);
+    pickedName = [];
+    var result = document.getElementById("result");
+    result.textContent = "RESULT";
+}
